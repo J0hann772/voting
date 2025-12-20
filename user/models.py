6081e00d796240835a1d729ec44d1b260b65f312
Evgeniy <gea548845@mail.ru>
2025-12-21 02:10:21 +0300

    created database

--- user/models.py
+++ user/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=40, blank=True, null=True)
+
+    USERNAME_FIELD = 'email' # вход по почте
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Профиль {self.user.username}"
+
+# профиль создается автоматически при регистрации пользователя
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+
+
+
+
--- voting/models.py
+++ voting/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Voting(models.Model): #Голосование
+
+    class VotingType(models.TextChoices):
+        SINGLE = 'single', 'Один вариант'
+        MULTIPLE = 'multiple', 'Несколько вариантов'
+
+
+    title = models.CharField(max_length=80)
+
+    voting_type = models.CharField(
+        max_length=10,
+        choices=VotingType.choices,
+        default=VotingType.SINGLE
+    )
+
+    max_votes = models.PositiveIntegerField(default=1, help_text="Максимум голосов от одного юзера")
+
+    description = models.CharField(max_length=200, blank=True, null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    likes = models.ManyToManyField(User, related_name='liked_votings', blank=True)
+
+
+class Choice(models.Model): #Вариант голоса
+
+    voting = models.ForeignKey(Voting, on_delete=models.CASCADE, related_name='choices')
+    text = models.CharField(max_length=300, null=False, blank=False)
+
+
+class Vote(models.Model): #Голос
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name='votes')
+
+    class Meta:
+        unique_together = ['author', 'choice']
+
+    def __str__(self):
+        return f"{self.author.username} проголосовал за {self.choice.text}"
+
+
+class Report(models.Model):
+
+    voting = models.ForeignKey(Voting, on_delete=models.CASCADE, related_name='reports')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    title = models.CharField(max_length=40, null=False, blank=False)
+    description = models.CharField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.author.username} оставил жалобу на {self.voting.title}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
